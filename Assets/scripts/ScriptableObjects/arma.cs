29b61e79708a262f6bddd79ea417e825e36f36c5
davi-s-silva <69742480+Davi-S-Silva@users.noreply.github.com>
2022-04-05 14:01:49 -0300

    criando inventario

---
+++ Assets/scripts/ScriptableObjects/arma.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Arma", menuName = "Inventario/Nova Arma")]
+public class arma : item
+{
+    [SerializeField]
+    private float _forcaCano, _forcaDefesa;
+    [SerializeField]
+    private int _qtdMunicao;
+    [SerializeField]
+    private GameObject _prefab;
+}
---
+++ Assets/scripts/ScriptableObjects/item.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public class item : ScriptableObject
+{
+    [SerializeField]
+    private string _nameItem;
+    [SerializeField]
+    private float _precoItem;
+    [SerializeField][TextArea]
+    private string _descricaoItem;
+    [SerializeField]
+    private Sprite _imgItem;
+}
