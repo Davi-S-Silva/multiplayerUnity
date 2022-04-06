60d1b915aceb5a13eec02ec3a4c5794f46d319e1
davi-s-silva <69742480+Davi-S-Silva@users.noreply.github.com>
2022-04-06 13:59:25 -0300

    update inventario
    eventos nos itens

---
+++ Assets/scripts/UI/UiItem.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+public class UiItem : MonoBehaviour, IPointerClickHandler
+{
+    [SerializeField]
+    private Image _image;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPointerClick(PointerEventData pointerEventData)
+    {
+        Debug.Log("contato: "+ pointerEventData.ToString());
+        Destroy(transform.gameObject);
+    }
+}
